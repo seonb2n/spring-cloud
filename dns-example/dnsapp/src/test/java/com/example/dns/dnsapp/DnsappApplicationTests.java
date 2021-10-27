5579aaf9c21692fc6486b02de74642f8ed910f91
seonb2n <626ksb@gmail.com>
2021-10-27 22:19:50 +0900

    pjt create and run test

---
+++ dns-example/dnsapp/src/test/java/com/example/dns/dnsapp/DnsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dns.dnsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
